957b468f377dcc5bcc99232af8dc5cd46b6df48e
Jonathan Erazo <127910778+jaerazoc1@users.noreply.github.com>
2023-03-17 15:05:57 -0500

    Add files via upload

---
+++ script input.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 16 18:14:19 2023
+
+@author: Usuario
+"""
+nombre1 = int(input ("Ingrese nombre 1: "))
+nombre2 = int(input("Ingrese nombre 2: "))
+resultado = nombre1+nombre2
+print("El resultado de sumar:", nombre1, "+",nombre2,"es:", resultado)
+
+number1 =int(input("Ingrese valor 1:"))
+number2 = int(input("Ingrese valor 2:"))
+number3 = int(input("Ingrese valor 3:"))
+resultado = number1+number2+number3
+print("La media de sumar:", number1,"+",number2,"+",number3,"es:",resultado)
